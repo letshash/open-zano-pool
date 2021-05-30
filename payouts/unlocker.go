a906d21918fe5082cd1172a1d3210ffd75878053
Viktor Majko <65866311+eskal@users.noreply.github.com>
2021-05-30 20:21:21 +0200

    Update unlocker.go

--- payouts/unlocker.go
+++ payouts/unlocker.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/math"
 
-	"github.com/zano-mining/open-zano-pool/rpc"
+	"github.com/letshash/open-zano-pool/rpc"
 	"github.com/zano-mining/open-zano-pool/storage"
 	"github.com/zano-mining/open-zano-pool/util"
 )
